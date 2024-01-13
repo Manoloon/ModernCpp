8ddc5af64bcae660328bc3638d3d00fb4e43a430
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-13 08:28:31 -0300

    Update ThreadSafeVector.h

--- src/ThreadSafeVector.h
+++ src/ThreadSafeVector.h
@@ -4,5 +4,14 @@
 #include<vector>
 #include<mutex>
 
+template class <typename T>
 class ThreadSafeVec{
+    std::mutex mut;
+    std::vector<T> vec;
+    public:
+    void push_back(const T& item){
+        mut.lock();
+        vec.push_back(item);
+        mut.unlock();
+    }
 };
