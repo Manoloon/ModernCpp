c479a33d258cbda24b7f1ad4b9be8f648a5ce40c
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-13 15:06:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 // Created by Manoloon on 28/12/2023.
 //
 #include <thread>
+#include <vector>
 #include "src/TimedMutex.h"
 #include "src/lockGuard.h"
 
 
 int main() {
+		std::vector<std::jthread> threads;
+		for(int i = 0; i<5;i++){
+					threads.emplace_back(i);
+		}
+for(auto t : threads){
+					
+		}
 	
 }
