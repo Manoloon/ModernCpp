c1738f4e1fab795faa6ca243d6eae9902277db96
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-13 15:10:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,11 @@
 #include <vector>
 #include "src/TimedMutex.h"
 #include "src/lockGuard.h"
-
-
+#include "src/ThreadSafeVector.h"
 int main() {
 		std::vector<std::jthread> threads;
+		std::vectors<std::string_view> strings{"abc","def","ghi","jkl","mno"};
 		for(int i = 0; i<5;i++){
-					threads.emplace_back(i);
-		}
-for(auto t : threads){
-					
-		}
-	
+					threads.emplace_back(lockguard::Print(strings[i]));
+		}	
 }
