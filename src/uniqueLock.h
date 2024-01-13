17abe507e9c704c0180f42fddfd2f46dafc27a24
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-13 14:49:31 -0300

    Update uniqueLock.h

--- src/uniqueLock.h
+++ src/uniqueLock.h
@@ -8,13 +8,23 @@
 #include <iostream>
 #include <mutex>
 #include <chrono>
+#include <string_view>
 
 namespace uniqueLock{
     using namespace std::chrono_literals;
     std::mutex mutex;
-    void Task1(){
-        std::lock_guard lck_guard(mutex) ;
-        std::this_thread::sleep_for(5s);
+    void Print(std::string_view str){
+        for(int i =0; i<5;i++){
+            try{   
+        	std::lock_guard lck_guard(mutex);
+            std::cout << str[0] << str[1] << str[2] << std::endl;
+            throw Exception;	
+        	std::this_thread::sleep_for(5s);
+            }
+            catch(const Exception& e){
+                std::cout << "Exception caught :" << e.what() << std::endl;
+            }
+        } 
     }
 }
 #endif //MODERNCPP_UNIQUELOCK_H
