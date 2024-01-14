d1bd56c478f42ccecdc510930685eb3b49a8d0f2
Manoloon <pablo.r.sanchez@hotmail.com>
2024-01-14 11:21:03 +0100

    tasks

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 //
 #include <thread>
 #include <vector>
+#include <string>
 #include "src/TimedMutex.h"
 #include "src/lockGuard.h"
 #include "src/ThreadSafeVector.h"
+
 int main() {
-		std::vector<std::thread> threads;
-		std::vector<std::string_view> strings{"abc","def","ghi","jkl","mno"};
+        std::vector<std::thread> threads;
+        std::vector<std::string> strings{"abc","def","ghi","jkl","mno"};
 		for(int i = 0; i<5;i++){
 					threads.emplace_back(LockGuard::Print(strings[i]));
 		}
