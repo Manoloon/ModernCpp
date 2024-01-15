62f4456c077116d1e7ab92fa7b7a9e388046b756
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-15 05:38:26 -0300

    Update DoubleCheckedLock.h

--- src/DoubleCheckedLock.h
+++ src/DoubleCheckedLock.h
@@ -1,1 +1,21 @@
 // when doing lazy initialization , we need to do a double checked lock to avoid data race , but we can face another data race due to obj construct order.
+
+#include <mutex>
+#include <thread>
+
+class Test{
+	public:
+    void Func(){ std::cout << "test func" << std::endl;}
+    }
+void Process(){
+	Test ptest = nullptr; // lazy init.
+    std::once_flag ptest_flag;
+    std::mutex mutex;
+    std::unique_lock uni_lck(mutex);
+    if(!ptest){
+    	std::call_once(ptest_flag[](){
+            ptest = new Test;});
+        uni_lck.unlock();
+        ptest->Func();
+        }
+}
